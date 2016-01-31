d189e7866bba1b618b25212e2b6a019d49568e2d
HyroVitalyProtago <hyrovitalyprotago@gmail.com>
2016-01-31 20:31:50 +0100

    label test

--- webvr/js/main.js
+++ webvr/js/main.js
@@ -96,6 +96,16 @@ function init() {
     scene.add(plane);
     */
 
+    scene.addObject(createLabel(
+        "HELLO WORLD",
+        window.innerWidth*.5,
+        window.innerHeight*.5,
+        0,
+        100,
+        "black",
+        "yellow"
+    ));
+
 /*
     var canvas1 = document.createElement('canvas');
     var context1 = canvas1.getContext('2d');
@@ -287,6 +297,41 @@ function fullscreen() {
     } else if (container.webkitRequestFullscreen) {
         container.webkitRequestFullscreen();
     }
+}
+
+function createLabel(text, x, y, z, size, color, backGroundColor, backgroundMargin) {
+    if(!backgroundMargin)
+        backgroundMargin = 50;
+    var canvas = document.createElement("canvas");
+    var context = canvas.getContext("2d");
+    context.font = size + "pt Arial";
+    var textWidth = context.measureText(text).width;
+    canvas.width = textWidth + backgroundMargin;
+    canvas.height = size + backgroundMargin;
+    context = canvas.getContext("2d");
+    context.font = size + "pt Arial";
+    if(backGroundColor) {
+        context.fillStyle = backGroundColor;
+        context.fillRect(canvas.width / 2 - textWidth / 2 - backgroundMargin / 2, canvas.height / 2 - size / 2 - +backgroundMargin / 2, textWidth + backgroundMargin, size + backgroundMargin);
+    }
+    context.textAlign = "center";
+    context.textBaseline = "middle";
+    context.fillStyle = color;
+    context.fillText(text, canvas.width / 2, canvas.height / 2);
+    // context.strokeStyle = "black";
+    // context.strokeRect(0, 0, canvas.width, canvas.height);
+    var texture = new THREE.Texture(canvas);
+    texture.needsUpdate = true;
+    var material = new THREE.MeshBasicMaterial({
+        map : texture
+    });
+    var mesh = new THREE.Mesh(new THREE.PlaneGeometry(canvas.width, canvas.height), material);
+    // mesh.overdraw = true;
+    mesh.doubleSided = true;
+    mesh.position.x = x - canvas.width;
+    mesh.position.y = y - canvas.height;
+    mesh.position.z = z;
+    return mesh;
 }
 
 window.addEventListener("gamepadconnected", function (e) {
