599100b85a879428b792dda5f84d6cc182110859
HyroVitalyProtago <hyrovitalyprotago@gmail.com>
2016-09-09 11:03:15 +0200

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ var simulation = d3.forceSimulation()
   .force("link", d3.forceLink().id(function(d) {
     return d.id;
   }))
-  .force("charge", d3.forceManyBody())
+  .force("charge", d3.forceManyBody().strength(-100))
   .force("center", d3.forceCenter(width / 4, height / 2));
 
 d3.json("sota.json", function(error, graph) {
